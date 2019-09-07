06fb0da66fbae40d43debb788786e2eb8ba01c1f
goodguydul <48383804+goodguydul@users.noreply.github.com>
2019-09-07 11:11:51 +0700

    submited

--- soal_2.php
+++ soal_2.php
@@ -1,41 +1,79 @@
-<!-- Pada sebuah form, terdapat 4 buah field yaitu: username dan password. Buatlah method/function untuk memvalidasi field-field tersebut, dengan requirement sebagai berikut:
-●       Disarankan menggunakan REGEX
-●       Format username: merupakan kombinasi dari huruf kecil, huruf besar dan angka dengan panjang 5-9 karakter. Username tidak boleh diawali dengan angka / karakter special.
-●       Format password: merupakan kombinasi dari huruf kecil, huruf besar minimal satu karakter, angka minimal satu karakter, dan karakter spesial minimal satu karakter dan harus memiliki karakter simbol “=”  dan panjang minimal 8 karakter.
-Clue:
-Peserta hanya diminta membuat function validasi, tidak perlu membuat form HTML.
-Examples:
--        is_username_valid(‘Xrutaf888’)
-    return true
--        is_username_valid(‘1Diana’)
-    return false
--        is_password_valid(‘passW0rd=’)
-    return true
--        is_password_valid(‘C0d3YourFuture!#’)
-    return false
- -->
 <?php 
+// Pada sebuah form, terdapat 2 buah field yaitu: username dan password. Buatlah method/function untuk memvalidasi field-field tersebut, dengan requirement sebagai berikut:
+// ●   	Disarankan menggunakan REGEX
+// ●   	Format username: merupakan kombinasi dari huruf kecil dengan panjang 5-9 karakter. Username tidak boleh terdapat huruf besar/angka/karakter special.
+// ●   	Format password: merupakan kombinasi dari huruf kecil, huruf besar minimal satu karakter, angka minimal satu karakter, dan karakter spesial minimal satu karakter. Panjang password minimal 8 karakter..
+// Clue:
+// Peserta hanya diminta membuat function validasi, tidak perlu membuat form HTML.
+// Examples:
+// -        is_username_valid(‘cod3r)
+// 	return false
+// -        is_username_valid(‘siska’)
+// 	return true
+// -        is_password_valid(‘p4s$gW’)
+// 	return false
+// -        is_password_valid(‘codeYourFuture!123)
+// 	return true
+
+
 	function is_usernameValid($username){
-		if (preg_match('/^[a-zA-Z][a-zA-Z0-9]{5,9}$/', $username)) {
-			
-			return 1;
+		if (preg_match('/^[a-z]{5,9}$/', $username)) {// regex : hanya menerima alphabet lowercase dengan panjang min 5 max 9
+
+			return true; // kalau sesuai, maka return true/1
 
 		}else{
 			
-			return 0;
+			return 0; // jika tidak maka return 0/false, kenapa menggunakan 0? karena kalau menggunakan false, maka return tidak akan menampilkan apa-apa
 		}
 	}
 	
 	function is_passwordValid($password){
-		if (preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.*[=])(?=.{8,})/', $password)) {
-			
-			return 1;
+		if (preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/', $password)) {
+			//regex : hanya menerima aplhabet lowercase, uppercase min 1 char, numeric 1 char, dan special char min 1. (?=.*) << lookaround positive. 
+			return true; // kalau sesuai, maka return true/1
 
 		}else{
 			
 			return 0;
 		}
 	}
-$x = is_passwordValid("Abdulganteng12!=");
+
+$x = is_passwordValid("abdulGanteng12$"); //return 1
 print_r($x);
+
+// Positive Lookahead (?=.*[a-z])
+// 	Assert that the Regex below matches
+// 	.* matches any character (except for line terminators)
+// 	* Quantifier — Matches between zero and unlimited times, as many times as possible, giving back as needed (greedy)
+// 	Match a single character present in the list below [a-z]
+// 	a-z a single character in the range between a (index 97) and z (index 122) (case sensitive)
+// Positive Lookahead (?=.*[A-Z])
+// 	Assert that the Regex below matches
+// 	.* matches any character (except for line terminators)
+// 	* Quantifier — Matches between zero and unlimited times, as many times as possible, giving back as needed (greedy)
+// 	Match a single character present in the list below [A-Z]
+// 	A-Z a single character in the range between A (index 65) and Z (index 90) (case sensitive)
+// Positive Lookahead (?=.*[0-9])
+// 	Assert that the Regex below matches
+// 	.* matches any character (except for line terminators)
+// 	* Quantifier — Matches between zero and unlimited times, as many times as possible, giving back as needed (greedy)
+// 	Match a single character present in the list below [0-9]
+// 	0-9 a single character in the range between 0 (index 48) and 9 (index 57) (case sensitive)
+// Positive Lookahead (?=.*[!@#\$%\^&\*])
+// 	Assert that the Regex below matches
+// 	.* matches any character (except for line terminators)
+// 	* Quantifier — Matches between zero and unlimited times, as many times as possible, giving back as needed (greedy)
+// 	Match a single character present in the list below [!@#\$%\^&\*]
+// 	!@# matches a single character in the list !@# (case sensitive)
+// 	\$ matches the character $ literally (case sensitive)
+// 	% matches the character % literally (case sensitive)
+// 	\^ matches the character ^ literally (case sensitive)
+// 	& matches the character & literally (case sensitive)
+// 	\* matches the character * literally (case sensitive)
+// Positive Lookahead (?=.{8,})
+// 	Assert that the Regex below matches
+// 	.{8,} matches any character (except for line terminators)
+// 	{8,} Quantifier — Matches between 8 and unlimited times, as many times as possible, giving back as needed (greedy)
+
+
 ?>
--- soal_3.php
+++ soal_3.php
@@ -1,46 +1,28 @@
-<!-- Segitiga siku-siku adalah segitiga yang salah satu sudutnya 90o atau sudut siku. Buatlah sebuah program untuk membentuk sebuah segitiga siku-siku.
- 
-
-REQUIREMENT:
-Input user berupa panjang alas dan tinggi segitiga (alas dan tingginya sama)dengan ketentuan :
-0 < Alas/Tinggi < 10
-Segitiga dibentuk dengan angka prima yang nilainya mulai dari awal setiap barisnya.
-Output berupa segitiga siku-siku.
- -->
-
-
 <?php 
-	
-	function getPrime($n){
-		$max = 0;
-		$x	 = 2;
-		while ($max<$n) {
-			$prima = true;
-			for ($i=2; $i < $x ; $i++) { 
-				if ($x % $i == 0) {
-					$prima = false;		
+// Buatlah method/function untuk mencetak segitiga seperti gambar berikut. Parameter yang diberikan adalah jumlah baris dengan ketentuan minimal 3. Jumlah karakter di baris pertama merupakan ganjil. 
+
+	function printTriangle($n){
+		for ($i=$n; $i >=1 ; $i--) { 
+
+			// looping untuk ruang kosong ditengah
+			for ($ruang=$i; $ruang < $n ; $ruang++) { 
+				echo " ";
+			}
+
+			// looping untuk bintang-bintang
+			for ($star=1; $star < 2*$i; $star++) {  
+				if($i == $n || $star == 1 || $star == 2*$i - 1){
+		            echo  "*";
 				}
-			}if ($prima == true) {
-				echo $x;
-				$max++;
-			}$x++;
+		        else{
+		            echo  " ";
+		        }
+			}
+			echo "\n";
 		}
 	}
-
-	function segitigaSikuSikuPrima($max){
-		for ($n=1; $n <= $max; $n++) {
-			$a = 0;
-			if ($max<10) {
-				
-				getPrime($n);
-				echo "<br>";
-				
-			}else{
-				break;
-			}	
-		} 	
-	}
-
-	echo segitigaSikuSikuPrima(5);
 	
+	printTriangle(4);
+
+	//nb : di soal tertera "printTriangle(5)" namun outputnya ada 4 baris. berarti soal tersebut typo.
 ?>
